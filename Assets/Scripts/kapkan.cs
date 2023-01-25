e479b8da039c19e066e5f19f65df1316017dc259
korobyaken <58810125+korobyaken@users.noreply.github.com>
2023-01-25 21:16:59 +0700

    all files

--- Assets/Scripts/kapkan.cs
+++ Assets/Scripts/kapkan.cs
@@ -1,9 +1,12 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class kapkan : MonoBehaviour
 {
+    public GameObject gp;
+    public float force;
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "Player")
